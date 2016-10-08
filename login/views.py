27f70c3efa2d8f103f70ed1f3b902f6d0787aad3
Osmar Coronel <OsmarCoronel@wirelessprv-10-192-21-54.near.illinois.edu>
2016-10-08 02:16:24 -0500

    Cleaning up project

--- login/views.py
+++ login/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth import logout
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
-from django.template import RequestContext
 from django.contrib.auth.models import User
 from CompleteApp import views
 
