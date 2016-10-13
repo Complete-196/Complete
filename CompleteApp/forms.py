9d8b0bbce3eaa8fc0e3338f41f6a1a80e1525e7c
Osmar Coronel <OsmarCoronel@wirelessprv-10-192-21-54.near.illinois.edu>
2016-10-13 18:30:33 -0500

    Did some cleaning of code.

--- CompleteApp/forms.py
+++ CompleteApp/forms.py
@@ -5,10 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 #(\w+\s\w+)
 class NewEventForm(forms.Form):
 
-    title = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
+    title = forms.RegexField(regex=r'(\w+\s\w+)+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
                                 label=_("Title"), error_messages={
             'invalid': _("This value must contain only letters, numbers and underscores.")})
-    duration = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
+    duration = forms.RegexField(regex=r'(\w+\s\w+)+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
                                 label=_('Duration'),error_messages={
         'invalid': _("This value must contain only letters, numbers and underscores.")})
     #due = forms.DateField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Due"))
