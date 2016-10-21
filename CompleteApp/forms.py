cfcf15d7b280d70077cf9761f29a482bcb5af264
Osmar Coronel <OsmarCoronel@wirelessprv-10-192-21-54.near.illinois.edu>
2016-10-21 18:20:33 -0500

    database spring cleaning

--- CompleteApp/forms.py
+++ CompleteApp/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 #(\w+\s\w+)
 class NewEventForm(forms.Form):
 
-    title = forms.RegexField(regex=r'(\w+\s\w+)+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
+    title = forms.RegexField(regex=r'$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
                                 label=_("Title"), error_messages={
             'invalid': _("This value must contain only letters, numbers and underscores.")})
     duration = forms.RegexField(regex=r'(\w+\s\w+)+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)),
