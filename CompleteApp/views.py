99928471bfd4a048c93b3c813cf64c6d8c7bc4fb
Osmar Coronel <OsmarCoronel@wirelessprv-10-192-21-54.near.illinois.edu>
2016-10-25 04:08:56 -0500

    created some algorithms, still need to fix date time editing. everything else seems to be working fine.

--- CompleteApp/views.py
+++ CompleteApp/views.py
@@ -1,4 +1,5 @@
-
+import time
+import sys
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -11,7 +12,8 @@ from CompleteApp.forms import *
 
 @login_required
 def index(request):
-    query_results = Tasks.objects.filter(user=request.user)#values_list('title', flat='True')
+
+    query_results =  organizer(list(Tasks.objects.filter(user_id=request.user).order_by('dueTime')))
     return render_to_response("index.html",
                                {"query_results" : query_results, 'user':request.user})
 
@@ -24,7 +26,7 @@ def addview(request):
                     user=request.user,
                     title= form.cleaned_data['title'],
                     dueTime= request.POST.get('due'),
-                    duration= form.cleaned_data['duration'],
+                    duration= duration(request.POST.get('Days'), request.POST.get('Hours'),request.POST.get("Minutes"))#form.cleaned_data['duration'],
                 )
                 model.save()
                 return HttpResponseRedirect('/CompleteApp')
@@ -34,6 +36,23 @@ def addview(request):
         form = NewEventForm()
 
     return render(request, 'newEvent.html', {'form': form})
+
+def duration(days, hours, minutes):
+    if len(days)<1:
+        days = 0
+    if len(hours)<1:
+        hours = 0
+    if len(minutes)<1:
+        minutes=0
+    days = 1+int(days)
+    if days < 10:
+        days= '0'+str(days)
+    if int(hours) < 10:
+        hours = '0'+ hours
+    if int(minutes) < 10:
+        minutes = '0'+minutes
+    return str(days)+' '+hours+':'+minutes
+
 @login_required
 def delete(request,id):
     server = Tasks.objects.get(uniqueId=id).delete()
@@ -67,3 +86,34 @@ def handler404(request):
     response.status_code = 404
     return response
 '''
+
+
+
+def toEpoch(date):
+    if len(date) < 19:
+        pattern = '%Y-%m-%d %H:%M'
+    else:
+        pattern = '%Y-%m-%d %H:%M:%S'
+    return int(time.mktime(time.strptime(date, pattern)))
+
+def deadline(due, duration):
+
+    base = time.strftime('%Y-%m-', time.localtime(0))
+    duration = base + duration
+    epochDuration = toEpoch(duration)
+    epochDue = toEpoch(due)
+    deadline = epochDue-epochDuration
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(deadline))
+
+
+def organizer(a):
+    x = sys.maxint
+    for k in reversed(a):
+        i = str(k.dueTime)[:16]
+        j = str(k.duration)[:16]
+        if toEpoch(i) > x:
+            i = time.strftime('%Y-%m-%d %H:%M', time.localtime(x))
+        k.dueTime = deadline(i,j)
+
+        x = toEpoch(deadline(i, j))
+    return a
