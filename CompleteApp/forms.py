d2218eccfc8190921b45ea6801a1c3dc099d1273
Alex O'Kennard <alexoken@icloud.com>
2016-12-03 21:38:02 -0600

    Finished change password

--- CompleteApp/forms.py
+++ CompleteApp/forms.py
@@ -12,3 +12,16 @@ class NewEventForm(forms.Form):
     #                             label=_('Duration'),error_messages={
     #     'invalid': _("This value must contain only letters, numbers and underscores.")})
     #
+class RegistrationForm(forms.Form):
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
+        label=_("Password (again)"))
+
+    def clean(self):
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_("The two password fields did not match."))
+        return self.cleaned_data
--- CompleteApp/urls.py
+++ CompleteApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^addNew/$', views.addview),
     url(r'^delete/(?P<id>[\w-]+)/$',views.delete, name='delete'),
     url(r'^edit/(?P<id>[\w-]+)/$',views.edit, name='edit'),
+    url(r'^password/$', views.password, name='password'),
 ]
